129e88dbcd55bf13dfa694274f57ffc938f2b329
A-ryu <sanchezaaron315@gmail.com>
2022-02-12 13:26:28 +0800

    style-login

--- login.php
+++ login.php
@@ -3,12 +3,14 @@
     include('header.php');
  ?>
 <div class="container login">
-    <form action="includes/login.inc.php" method="post">
-        <input type="text" name="username" placeholder="Username/Email..." required>
-        <input type="password" name="password" placeholder="Password" required>
-        <input type="submit" name="submit" value="Login">
-        <a href="forgot-password.php">Forgot Password?</a>
-    </form>
+    <div class="login-form">
+        <form action="includes/login.inc.php" method="post"><br><br><br><br><br><br><br><br>
+            <input type="text" name="username" placeholder="Username/Email..." required><br><br>
+            <input type="password" name="password" placeholder="Password" required><br><br>
+            <input type="submit" name="submit" value="Login"><br><br>
+            <a href="forgot-password.php">Forgot Password?</a>
+        </form>
+    </div>
     <?php
         if (isset($_GET["error"])) {
             if ($_GET['error'] == "wrongaccount") {
